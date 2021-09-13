4625c8cf556b0f75cedd3222dd6e7da02c16d0c1
jkh5039 <borm3403@naver.com>
2021-09-13 16:56:49 +0900

    initial commit

---
+++ BJ/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int A = int.Parse(ss[0]);
+            int B = int.Parse(ss[1]);
+            if (A > B)
+            {
+                Console.WriteLine(">");
+            }
+            else if (A == B)
+            {
+                Console.WriteLine("==");
+            }
+            else
+            {
+                Console.WriteLine("<");
+            }            
+            
+        }
+    }
+}
