b565118e9e9128b1c6640e55b6533a4b237e0313
jkh5039 <borm3403@naver.com>
2021-09-14 16:19:41 +0900

    ABCDE

--- BJ/ConsoleApp2/Program.cs
+++ BJ/ConsoleApp2/Program.cs
@@ -6,7 +6,32 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s = Console.ReadLine();           
+            int Score = Convert.ToInt32(s);
+            //@@
+            if(Score>=90&& Score <= 100)
+            {
+                Console.WriteLine("A");
+            }
+            if (Score >= 80 && Score <= 89)
+            {
+                Console.WriteLine("B");
+            }
+            if (Score >= 70 && Score <= 79)
+            {
+                Console.WriteLine("C");
+            }
+            if (Score >= 60 && Score <= 69)
+            {
+                Console.WriteLine("D");
+            }
+            if (Score >= 0 && Score <= 59)
+            {
+                Console.WriteLine("F");
+            }
+
+
+
         }
     }
 }
