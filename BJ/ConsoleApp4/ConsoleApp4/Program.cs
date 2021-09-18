fb08efa085ccca63ca1c011f7befe85d630a35f1
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-09-18 20:46:27 +0900

    Year

---
+++ BJ/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split(' ');
+            int H = int.Parse(a[0]);
+            int M = int.Parse(a[1]);
+
+            if (M >= 45)
+            {
+                M = M - 45;
+            }
+            else if (H >= 1 && M < 45)
+            {
+                H = H - 1;
+                M = M + 60 - 45;
+            }
+            else if (H == 0 && M < 45)
+            {
+                H = H + 24 - 1;
+                M = M + 60 - 45;
+            }
+
+            Console.WriteLine("{0} {1}", H, M);
+
+        }
+    }
+}
---
+++ BJ/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Year = int.Parse(Console.ReadLine());
+            if (Year % 4 == 0 && Year%100!=0 || Year%400==0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
