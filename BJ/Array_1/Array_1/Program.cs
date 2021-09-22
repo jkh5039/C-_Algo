c6061c50a5253398bfffb5f125342362905af35b
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-09-22 18:17:14 +0900

    야qkf

---
+++ BJ/Array_1/Array_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int[] nums = new int[T];
+            string[] Arrays = Console.ReadLine().Split();
+
+            for(int i=0; i<nums.Length; i++)
+            {
+                nums[i] = int.Parse(Arrays[i]);
+            }
+
+            Array.Sort(nums);
+            //Console.WriteLine($"{nums[0]} {nums[nums.Length - 1]}");
+            Console.WriteLine(nums[0] + " " + nums[nums.Length - 1]);
+        }
+    }
+}
