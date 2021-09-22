5606118c69e49124202caad60e51b432dd6ddf4d
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-09-22 22:16:55 +0900

    PracticeNottte

---
+++ BJ/PracticeNotes/PracticeNotes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2.0f - 1.1f);
+        }
+    }
+}
