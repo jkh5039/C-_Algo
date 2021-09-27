526abffbb7e9e4fc547fd7fc381374b3840ed3d8
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-09-27 20:06:42 +0900

    asd

--- BJ/PracticeNotes/PracticeNotes/Program.cs
+++ BJ/PracticeNotes/PracticeNotes/Program.cs
@@ -2,11 +2,31 @@
 
 namespace PracticeNotes
 {
+
+    
     class Program
     {
+
+        static int Solution(int[] a, int[] b)
+        {
+
+            int answer = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                int sum = a[i] * b[i];
+                answer += sum;
+            }
+            return answer;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(2.0f - 1.1f);
+            int[] a = { 1, 2, 3 };
+            int[] b = { 1, 2, 3 };            
+            Console.WriteLine(Solution(a,b));
+
         }
+        
+        
     }
 }
---
+++ BJ/Str_1/Str_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Str_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = Convert.ToChar(Console.ReadLine());
+            int ascii = Convert.ToInt32(a);
+            Console.WriteLine(ascii);
+        }
+    }
+}
