96ec4a0c9e805e814c2f4f24d529fcf295821616
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-10-10 19:12:11 +0900

    asd

---
+++ BJ/Array4/Array4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Array4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////서로 다른 나머지 갯수
+            //int count = 0;
+            ////입력받을 10개의 숫자
+            //int[] Result = new int[10];
+            ////입력받은 10개의 숫자
+            //for(int i=0; i<10; i++)
+            //{
+            //    //곱해질수 2개
+            //    int A = int.Parse(Console.ReadLine());
+            //    int B = int.Parse(Console.ReadLine());
+            //    //곱하고 42로 나누어진 결과 
+            //    Result[i]= (A * B) % 42;             
+
+
+            //}
+            ////i번째숫자와 다른숫자들 비교한다                
+            ////만약 나머지가 서로 다르다면 count를 추가한다 
+
+            ////? 했던건 건드리면 안된다.
+            //for (int j=0; j < 9;j++)
+            //{
+            //    for(int k=1; k <= 9; k++)
+            //    {
+            //        if (Result[j] != Result[k])
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine(count);
+
+            const int CASE = 42;
+
+            //Input part
+            bool[] answer = new bool[CASE];
+            //Cases True
+            for (int i = 0; i < 10; i++)
+                answer[int.Parse(Console.ReadLine()) % CASE] = true;
+
+            int count = 0;
+            //BoolCount 
+            for (int i = 0; i < CASE; i++)
+                if (answer[i])
+                    count++;
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ BJ/Array_3/Array_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Array_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int num = A * B * C;
+            string Nums = num.ToString();
+            //int[] Answer = new int[10];
+            int count = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j=0; j < Nums.Length; j++)
+                {
+                    if (Nums[j].ToString() == i.ToString())
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                count = 0;
+            }
+
+        }
+    }
+}
