d61cf78f6352f96bc58dd8a5127a9c87d0dcd789
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-10-21 23:43:22 +0900

    str4

---
+++ BJ/String_3/String_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+namespace String_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            
+            for (int i=0; i< count; i++)
+            {
+                string[] Info = Console.ReadLine().Split();
+                int num = int.Parse(Info[0]);
+                string str = Info[1];
+                for (int j=0; j<str.Length; j++)
+                {
+                    
+                    for (int k=0; k < num; k++)
+                    {
+                        Write(str[j]);
+                    }
+                }
+                Write("\n");
+            }
+            
+
+        }
+    }
+}
