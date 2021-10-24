2a2e6b994b2b3a3f9ddc6da9c076d18e856626f7
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-10-24 19:58:25 +0900

    asd

---
+++ BJ/String_7/String_7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+
+namespace String_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine().Split();
+
+            //Console.WriteLine(nums.Length);
+            string num1 = new string(nums[0].ToCharArray().Reverse().ToArray());
+            string num2 = new string(nums[1].ToCharArray().Reverse().ToArray());
+            int number1 = int.Parse(num1);
+            int number2 = int.Parse(num2);
+            if (number1 > number2)
+            {
+                Console.WriteLine(number1);
+            }
+            else if (number2 > number1)
+            {
+                Console.WriteLine(number2);
+            }
+
+
+
+
+
+        }
+    }
+}
