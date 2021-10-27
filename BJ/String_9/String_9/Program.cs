37935a81841634bf7ef6414a4caf2c3d885e27ad
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-10-27 21:38:37 +0900

    #2941

---
+++ BJ/String_9/String_9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace String_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Croatia = Console.ReadLine();
+            //int worldLen = Croatia.Length;
+            string[] Cword = { "dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z=" };
+            for(int i=0; i < Cword.Length; i++)
+            {
+                if (Croatia.Contains(Cword[i]))
+                {
+                    Croatia = Croatia.Replace(Cword[i], "0");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+
+            //Console.WriteLine(Croatia);
+            Console.WriteLine(Croatia.Length);
+        }
+    }
+}
