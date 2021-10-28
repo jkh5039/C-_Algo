eeee612a6c34a6759b02e6f2bed035168071ed37
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-10-28 23:44:04 +0900

    #1712

---
+++ BJ/BasicMath1_1/BasicMath1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicMath1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] point = Console.ReadLine().Split();
+            int A = int.Parse(point[0]);
+            int B = int.Parse(point[1]);
+            int C = int.Parse(point[2]);
+
+            if (C - B <= 0) Console.WriteLine(-1);
+            else 
+            {
+                Console.WriteLine((A / (C - B)) + 1);
+            }
+        }
+    }
+}
