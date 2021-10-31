282b4efd5d9becb74c95347b2cbc26a8411f2767
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-10-31 17:10:17 +0900

    #2292

---
+++ BJ/BasicMath1_2/BasicMath1_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BasicMath1_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            int x = int.Parse(Console.ReadLine());
+
+            int pos = 0;
+            int range = 1;
+            int sequence = 1;
+            //if (x == 1)
+            //{
+            //    Console.WriteLine(1);
+            //}
+
+            while (true)
+            {
+                pos++;
+                if (range >=x)
+                {
+                    break;
+                }
+                
+                sequence = 6 * pos;
+                range += sequence;
+
+            }
+
+            if (x == 1)
+            {
+                pos = 1;
+            }
+            Console.WriteLine(pos);
+            
+        }
+    }
+}
