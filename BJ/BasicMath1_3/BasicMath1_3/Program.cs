6d228b4783a3672e8ce943af26b70c98ecaf45f7
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-01 23:41:20 +0900

    미완

---
+++ BJ/BasicMath1_3/BasicMath1_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BasicMath1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] snail = Console.ReadLine().Split();
+            int days = 0;
+            int meters = 0;
+            int A = int.Parse(snail[0]);
+            int B = int.Parse(snail[1]);
+            int V = int.Parse(snail[2]);
+
+
+            while (true)
+            {
+                days++;
+                meters += A;
+                if (meters >= V)
+                {
+                    break;
+                }
+                meters -= B;              
+                
+                
+                
+                
+                
+            }
+            Console.WriteLine(days);
+        }
+    }
+}
