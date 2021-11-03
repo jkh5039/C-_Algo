cd5d57d3c4a3d7c90fc97fb01838d20d4073182d
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-03 22:33:17 +0900

    Hotel

---
+++ BJ/BasicMath1_5/BasicMath1_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BasicMath1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int T = int.Parse(Console.ReadLine());
+            
+            for(int i=0; i<T; i++)
+            {
+
+                string[] Hotel = Console.ReadLine().Split();
+                int H = int.Parse(Hotel[0]); //~99
+                int W = int.Parse(Hotel[1]); //~99
+                int customer = int.Parse(Hotel[2]);
+
+                //층수 알려준다
+                int Layer = customer % H;
+                
+
+                int room = customer / H;
+                room += 1;
+                if (Layer == 0)
+                {
+                    //제일 꼭대기층 
+                    Layer = H;
+                    room -= 1;
+                }
+                string Layer1 =Layer.ToString();
+                string room1 = room.ToString("D2");
+                
+
+                Console.WriteLine(Layer1+room1);
+            }
+            
+            
+            
+
+        }
+    }
+}
