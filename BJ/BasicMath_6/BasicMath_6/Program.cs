b14aecae2e5bd1deced9da78ab91d06a86835758
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-08 22:19:57 +0900

    x

--- BJ/BasicMath_6/BasicMath_6/Program.cs
+++ BJ/BasicMath_6/BasicMath_6/Program.cs
@@ -7,17 +7,39 @@ namespace BasicMath_6
         static void Main(string[] args)
         {
             int x = int.Parse(Console.ReadLine());
+            
             int layer = 1;
             int num = 1;
-            while (num > x)
+
+            int denominator=0; //분모 
+            int numerator=0; //분자
+            while (num < x)
             {
                 //층수 올라감
                 layer++;
                 //층수의 최대숫자
                 num += layer; 
             }
-            int start = num - layer + 1; //분모시작 최소숫자
-            int last = num; //해당 레이어 마지막 숫자.
+            int start = num - x + 1;   
+            //start 
+            //만약 레이어가 짝수이면 분모가 큰상태로 시작 
+            
+            if (layer % 2 == 0)
+            {
+                numerator = layer+1-start;
+                denominator = start;
+
+                
+            }
+            if(layer % 2 == 1)
+            {
+                numerator = start;
+                denominator = layer + 1 - start;
+            }
+            //만약 홀수라면 홀수라면 분자가 큰상태로 시작
+            //int last = num; //해당 레이어 마지막 숫자.
+            Console.WriteLine(numerator+"/"+ denominator);
+            
 
         }
     }
