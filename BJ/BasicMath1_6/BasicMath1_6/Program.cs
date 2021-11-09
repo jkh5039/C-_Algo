ad8657bf2670e5ede328d03b4be0f1914d26997f
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-09 22:45:50 +0900

    donno  2775

---
+++ BJ/BasicMath1_6/BasicMath1_6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BasicMath1_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.acmicpc.net/problem/2775
+
+
+            int T = int.Parse(Console.ReadLine());
+            for(int i=0; i < T; i++)
+            {
+                int k = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());
+                int start = 1;
+                int sum = 1;
+                //호수
+                int room = 1;
+                //층수
+                int layer = 0;
+                //1층에있는걸 계산할때 1층
+                while (room<n) //호수를 채운다
+                {
+                    start++;
+                    sum += start;
+                    room++;                   
+                    
+                }
+
+                //2층 다음푸터는 어떻게?
+                
+                
+                //1호는 다 1로 바꾼다
+                if (n == 1)
+                {
+                    sum = 1;
+                }
+                Console.WriteLine(sum);
+                
+                
+
+            }
+        }
+    }
+}
