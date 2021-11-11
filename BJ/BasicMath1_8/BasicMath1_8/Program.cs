15771e238cb49c4221d89995c4b6f0a232476c0e
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-11 23:19:06 +0900

    0

---
+++ BJ/BasicMath1_8/BasicMath1_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+namespace BasicMath1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split();
+            BigInteger x = BigInteger.Parse(num[0]);
+            BigInteger y = BigInteger.Parse(num[1]);
+            Console.WriteLine(x + y);
+        }
+    }
+}
