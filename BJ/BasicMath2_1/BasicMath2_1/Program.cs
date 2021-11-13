2fc4d4fd33077f709f82687918e1c5040a5fef15
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-13 22:38:57 +0900

    #1978 소수 찾기

---
+++ BJ/BasicMath2_1/BasicMath2_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicMath2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            string[] nums = Console.ReadLine().Split();
+            int count = nums.Length;
+            //Try
+            for(int i=0; i < T; i++)
+            {
+                if (int.Parse(nums[i]) == 1)
+                {
+                    count--;
+                }
+                // 2~ number itself-1
+                for (int j=2; j< int.Parse(nums[i])-1; j++)
+                {                    
+                    if ( int.Parse(nums[i])%j==0)
+                    {
+                        count--;
+                        break;
+                    }
+                }
+
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
