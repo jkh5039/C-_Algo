714592bf3481187e3a03be73aba4d167c9f8a40b
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-24 07:23:09 +0900

    #11653 s

---
+++ BJ/BJ/ConsoleApp1B/BasicMath2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicMath2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            //int K = N;
+            for(int i=2; i <= N; i++)
+            {
+                while (N % i == 0)
+                {
+                    Console.WriteLine(i);
+                    N = N / i;
+                }
+            }
+        }
+    }
+}
