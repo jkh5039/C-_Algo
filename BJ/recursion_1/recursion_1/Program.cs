59b3fd43a2379d6470f523c13b3ec96b2c2c5a1d
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-11-25 00:15:11 +0900

    #10872 & #10870 ^_^

---
+++ BJ/recursion_1/recursion_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace recursion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            if (N == 0)
+            {
+                factorial = 1;
+            }
+            
+            while (N > 1)
+            {
+                factorial *= N;
+                N--;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
---
+++ BJ/recursion_2/recursion_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace recursion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+
+            //n-2
+            int temp1 = 0;
+            //n-1
+            int temp2 = 1;
+            //n
+            int temp = 0;
+
+            int k = 0;
+
+            while (n - 1 > k)
+            {
+
+                //k번째 수 등장
+                temp = temp1 + temp2;
+                //k-2번째 수 배치
+                temp1 = temp2;
+                //k-1수 배치
+                temp2 = temp;
+                k++;
+
+            }
+            if (n == 0)
+            {
+                temp = 0;
+            }
+            if (n == 1)
+            {
+                temp = 1;
+            }
+            if (n == 2)
+            {
+                temp = 1;
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
