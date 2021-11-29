8b632f59c5269a27157f3ece33b3b20b9ab59468
jkh5039 <borm3403@naver.com>
2021-11-29 10:03:21 +0900

    #2750

---
+++ BJ/Sort_1/Sort_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sort_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] nums = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums[i] = num;
+            }
+            Array.Sort(nums);
+
+            for (int j = 0; j < N; j++)
+            {
+                Console.WriteLine(nums[j]);
+            }
+        }
+    }
+}
