d6847e3410301fdb6a852c57480931d64709c84d
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-12-08 22:03:55 +0900

    #1463 "666"

---
+++ BJ/bruteForce_5/bruteForce_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bruteForce_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int answer = 0;
+            int i = 0;
+            while (N > 0)
+            {                
+                if (i.ToString().Contains("666"))
+                {
+                    answer = i;
+                    N--;
+                }                
+                i++;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
