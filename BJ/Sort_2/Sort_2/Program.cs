3d61c655c4c9ee5dc946137c497116a74debfe2b
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-12-10 01:36:11 +0900

    #2751 sortNums

---
+++ BJ/Sort_2/Sort_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+namespace Sort_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] nums = new int[N];
+            for(int i = 0; i < N; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(nums);
+
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < N; j++)
+            {
+                sb.Append(nums[j]);
+                sb.Append("\n");
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
