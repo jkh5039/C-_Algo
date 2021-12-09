c6ea7dd3f074485163015a60209150cfb4add1f0
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-12-10 01:57:39 +0900

    #10989 SOS 도와줘요..

---
+++ BJ/Sort_3/Sort_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+namespace Sort_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger N = BigInteger.Parse(Console.ReadLine());
+            BigInteger[] nums = new BigInteger[N];
+            for (BigInteger i = 0; i < N; i++)
+            {
+                nums[i.Sign] = BigInteger.Parse(Console.ReadLine());
+            }
+            Array.Sort(nums);
+
+            StringBuilder sb = new StringBuilder();
+            for (BigInteger j = 0; j < N; j++)
+            {
+                sb.Append(nums[j]);
+                sb.Append("\n");
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
