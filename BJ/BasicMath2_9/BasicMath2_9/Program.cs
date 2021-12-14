e94bb7f3467a85f756f4a503ce5c795e7af6b617
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-12-14 22:24:26 +0900

    #4153 ㅗ삼각형

---
+++ BJ/BasicMath2_9/BasicMath2_9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicMath2_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                string[] nums = Console.ReadLine().Split();
+                
+                int x = int.Parse(nums[0]);
+                int y = int.Parse(nums[1]);
+                int z = int.Parse(nums[2]);
+                
+
+                if (x == 0 && y == 0 && z == 0)
+                    return;
+
+                if (x * x + y * y == z * z ||
+                    x * x + z * z == y * y ||
+                    y * y + z * z == x * x)
+                    Console.WriteLine("right");
+
+                else                
+                    Console.WriteLine("wrong");                
+            }
+            
+        }
+    }
+}
