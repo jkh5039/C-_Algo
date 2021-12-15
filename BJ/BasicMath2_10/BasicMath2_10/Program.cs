294e0503a44d7b50e3fce4968d0b252d88687818
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-12-16 00:06:09 +0900

    dontknow

---
+++ BJ/BasicMath2_10/BasicMath2_10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BasicMath2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int R = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.PI * R * R);
+            Console.WriteLine(2 * R * R);
+        }
+    }
+}
---
+++ BJ/Sort_8/Sort_8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+namespace Sort_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            string[] words = new string[T];
+            List<string> wordSort = new List<string>();
+            //Console.WriteLine(wordSort.Count);
+            int len = 1;
+            
+            for(int i = 0; i < T; i++)
+            {
+                string X = Console.ReadLine();
+                words[i] = X;
+            }
+                       
+
+            //알파벳순서대로 정렬
+            Array.Sort(words);
+
+            for(int p = 0; p < T; p++)
+            {
+                Console.WriteLine(words[p]);
+            }
+
+            while (wordSort.Count== T)
+            {
+                //Console.WriteLine("while들어감");
+
+                for (int j = 0; j < T; j++)
+                {
+                    if (words[j].Length == len)
+                    {
+                        wordSort.Add(words[j]);
+                        Console.WriteLine(wordSort.Count);
+                    }
+                    Console.WriteLine("들어감");
+                }
+                len++;
+            }
+
+            Console.WriteLine(wordSort.Count);
+            //for(int k = 0; k < T; k++)
+            //{
+            //    Console.WriteLine(wordSort[k]);
+            //}
+
+
+        }
+    }
+}
