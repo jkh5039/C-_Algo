c2288dffb20a5f6c6928ca0bd8ae44a1b856deb2
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-12-24 01:22:38 +0900

    오답.

---
+++ BJ/Array_7/Array_7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Array_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int sum=0;
+            float avg = 0;
+            float cnt = 0;
+            float result = 0;
+            for (int i = 0; i < A; i++)
+            {                
+                string[] nums = Console.ReadLine().Split();
+                int T = int.Parse(nums[0]);
+                for(int j = 1; j < nums.Length;j++)
+                {
+                    sum += int.Parse(nums[j]);
+                }
+                //Console.WriteLine(sum);
+                //Console.WriteLine(T);
+                avg = sum / T;
+                //Console.WriteLine(avg);
+                for(int k = 1; k < nums.Length; k++)
+                {
+                    if (int.Parse(nums[k]) > avg)
+                    {
+                        cnt++;
+                    }
+                }
+                //Console.WriteLine(cnt);
+                //Console.WriteLine(cnt / T);
+                result = (cnt / T) * 100;
+                double R = Math.Round(result, 3);
+                Console.WriteLine(R);
+            } 
+            
+        }
+    }
+}
