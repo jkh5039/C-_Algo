14ce634a6951e7947b7e75cbe600af096786b8c5
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2022-01-04 01:37:24 +0900

    배수약수 #5086

---
+++ BJ/NumTheory_1/NumTheory_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumTheory_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool go = true; //dont need
+            while (go)
+            {
+                String[] nums = Console.ReadLine().Split();
+                int x = int.Parse(nums[0]);
+                int y = int.Parse(nums[1]);
+
+                if (x == 0 && y == 0)
+                    return;
+
+                if (y % x == 0)
+                    Console.WriteLine("factor");
+                else if ((x % y == 0))
+                    Console.WriteLine("multiple");
+                else
+                    Console.WriteLine("neither");
+            }
+            
+
+
+
+
+        }
+    }
+}
