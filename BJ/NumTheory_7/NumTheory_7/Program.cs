314196ca4b638ee773c6e2e25c5475bf59e87082
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2022-01-04 01:54:10 +0900

    이항계수

---
+++ BJ/NumTheory_7/NumTheory_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumTheory_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine().Split();
+            int N = int.Parse(nums[0]);
+            int bunja=1;
+            int K = int.Parse(nums[1]);
+            int bunmo = 1;
+            for(int i = 0; i < K; i++)
+            {
+                bunja *= N;
+                N--;
+            }
+            while (K != 0)
+            {
+                bunmo *= K;
+                K--;
+            }
+            Console.WriteLine(bunja / bunmo);
+        }
+    }
+}
