038523d67ebf795b8156a83cde19a997c5ec5901
jkh5039 <borm3403@naver.com>
2022-01-05 14:41:35 +0900

    수정.

--- BJ/Queue_10845/Queue_10845/Program.cs
+++ BJ/Queue_10845/Queue_10845/Program.cs
@@ -1,69 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+
 namespace Queue_10845
 {
+    public static class Queue
+    {
+        public static uint back(this Queue<uint> q)
+        {
+            List<uint> l = new List<uint>();
+            for(int i =0;i< q.Count-1; ++i)
+            {
+                l.Add(q.Dequeue());
+            }
+            var temp = q.Dequeue();
+            l.Add(temp);
+
+            foreach(var i in l)
+            {
+                q.Enqueue(i);
+            }
+
+            return temp;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             int T = int.Parse(Console.ReadLine());
-            List<int> queue = new List<int>();
+            Queue<uint> queue = new Queue<uint>();
+
+            StringBuilder sb = new StringBuilder();
+
+            if (T < 1)
+                return;
+            if (T > 10000)
+                return;
+
             for (int i = 0; i < T; i++)
             {
                 string[] command = Console.ReadLine().Split();
-                if (command[0] == "push")
+                string cm = command[0];
+
+                switch (cm)
                 {
-                    queue.Add(int.Parse(command[1]));
-                }
+                    case "push":
+                        queue.Enqueue(uint.Parse(command[1]));
+                        break;
 
-                else if (command[0] == "pop")
-                {
-                    if (queue.Count > 0)
-                    {
-                        Console.WriteLine(queue[0]);
-                        queue.Remove(queue[0]);
+                    case "pop":
+                        if (queue.Count > 0)
+                        {
+                            Console.WriteLine(queue.Dequeue().ToString());
+                            //sb.Append(queue.Dequeue().ToString()+"\n");
+                        }
+                        else if (queue.Count == 0)
+                            Console.WriteLine("-1");
+                        break;
 
-                    }
+                    case "front":
+                        if (queue.Count > 0)
+                            Console.WriteLine(queue.Peek().ToString());
+                        else if (queue.Count == 0)
+                            Console.WriteLine("-1");
 
-                    else if (queue.Count == 0)
-                    {
-                        Console.WriteLine(-1);
-                        continue;
-                    }
-                }
+                        break;
 
-                else if (command[0] == "size")
-                {
-                    Console.WriteLine(queue.Count);
-                }
+                    case "back":
+                        if (queue.Count > 0)
+                            Console.WriteLine(queue.back().ToString());
+                        else if (queue.Count == 0)
+                            Console.WriteLine("-1");
 
-                else if (command[0] == "empty")
-                {
-                    if (queue.Count == 0)
-                        Console.WriteLine(1);
-                    else if (queue.Count > 0)
-                        Console.WriteLine(0);
+                        break;
 
-                }
+                    case "size":
+                        Console.WriteLine(queue.Count.ToString());
+                        break;
 
-                else if (command[0] == "front")
-                {
-                    if (queue.Count > 0)
-                        Console.WriteLine(queue[0]);
-                    else if (queue.Count == 0)
-                        Console.WriteLine(-1);
+                    case "empty":
+                        if (queue.Count == 0)
+                            Console.WriteLine("1");
 
-                }
+                        else if (queue.Count > 0)
+                            Console.WriteLine("0");
 
-                else if (command[0] == "back")
-                {
-                    if (queue.Count > 0)
-                        Console.WriteLine(queue[queue.Count - 1]);
-                    else if (queue.Count == 0)
-                        Console.WriteLine(-1);
-
+                        break;
                 }
             }
+
+            Console.WriteLine(sb.ToString());
         }
     }
 }
